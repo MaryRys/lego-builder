6aa7a2758c89862c1a645c5d3e258b745a408fcf
Mary Rys <rys.mary@gmail.com>
2018-11-11 12:19:22 -0600

    all things printing

--- src/components/LegoCharacter/legoCharacter.js
+++ src/components/LegoCharacter/legoCharacter.js
@@ -1,43 +1,85 @@
+import $ from 'jquery';
+
 import './legoCharater.scss';
+import partsData from '../../data/partsData';
 
-const createHeads = (data) => {
-  const domString = `
-  <div class="lego-card">
-    <div class="img-holder">
-      <img class= "${data.imageUrl}"
-    </div>
-    <h3 class="head-name"${data.name}</h3>
-  </div>
-  `;
-  return domString;
+// heads
+
+const createHeads = (heads) => {
+  let domString = '';
+  heads.forEach((head) => {
+    domString += `
+      <div class="lego-card">
+          <img class= "card" src="${head.imageUrl}" width="250px">
+      </div>
+      `;
+  });
+  $('#heads').html(domString);
 };
 
-const createTorsos = (data) => {
-  const domString = `
-  <div class="lego-card">
-    <div class="img-holder">
-      <img class= "${data.imageUrl}"
-    </div>
-    <h3 class="head-name"${data.name}</h3>
-  </div>
-  `;
-  return domString;
+const printHeads = () => {
+  partsData.getHeads()
+    .then((data) => {
+      (createHeads(data.data));
+    })
+    .catch((error) => {
+      console.error('Error getting heads', error);
+    });
 };
 
-const createLegs = (data) => {
-  const domString = `
-  <div class="lego-card">
-    <div class="img-holder">
-      <img class= "${data.imageUrl}"
-    </div>
-    <h3 class="head-name"${data.name}</h3>
-  </div>
-  `;
-  return domString;
+// torsos
+
+const createTorsos = (torsos) => {
+  let domString = '';
+  torsos.forEach((torso) => {
+    domString += `
+      <div class="lego-card">
+          <img class= "card" src="${torso.imageUrl}" width="250px">
+      </div>
+      `;
+  });
+  $('#torsos').html(domString);
+};
+
+const printTorsos = () => {
+  partsData.getTorsos()
+    .then((data) => {
+      (createTorsos(data.data));
+    })
+    .catch((error) => {
+      console.error('Error getting torsos', error);
+    });
+};
+
+// legs
+
+const createLegs = (legs) => {
+  let domString = '';
+  legs.forEach((leg) => {
+    domString += `
+      <div class="lego-card">
+          <img class= "card" src="${leg.imageUrl}" width="250px">
+      </div>
+      `;
+  });
+  $('#legs').html(domString);
+};
+
+const printLegs = () => {
+  partsData.getLegs()
+    .then((data) => {
+      (createLegs(data.data));
+    })
+    .catch((error) => {
+      console.error('Error getting legs', error);
+    });
 };
 
 export default {
   createHeads,
   createTorsos,
   createLegs,
+  printHeads,
+  printTorsos,
+  printLegs,
 };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-import $ from 'jquery';
-import 'bootstrap';
 import './index.scss';
 
-import partsData from 'partsData';
-import legoCharacters from './components/LegoCharacter';
-
-const printHeads = () => {
-  partsData.getHeads()
-  .then((data) => {
-    console.log(data);
-    legoCharacter.createHeads(data.heads);
-    $('#heads').html(legoCharacter.createHeads(data.heads));
-  })
-  .catch((error) => {
-    console.error('Error getting heads', error);
-  });
-};
+import legoCharacter from './components/LegoCharacter/legoCharacter';
 
 const initializeApp = () => {
-  printHeads();
+  legoCharacter.printHeads();
+  legoCharacter.printTorsos();
+  legoCharacter.printLegs();
 };
 
 initializeApp();
